74ce889508725a9ee6623dfd52ca5936bc57839d
Moin Iqbal <moiniqbal205@gmail.com>
2021-03-14 21:04:19 +1100

    1st commit: Vue Dark Mode toggle component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div 
+    class="app"
+    :class="mode"
+  >
+    <Header 
+      :mode="mode"
+      @toggle="toggle" 
+    />
+    <Content :mode="mode" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
+import Content from './components/Content';
 
 export default {
   name: 'App',
+  data () {
+    return {
+      mode: 'light'
+    }
+  },
   components: {
-    HelloWorld
+    Header,
+    Content
+  },
+  created () {
+    window.addEventListener('keyup', this.keyPress);
+  },
+  methods: {
+    keyPress (e) {
+      if (e.key === 't' || e.key === 'T') {
+        this.toggle();
+      }
+    },
+    toggle () {
+      if (this.mode === 'dark') {
+        this.mode = 'light';
+      } else {
+        this.mode = 'dark';
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background: #f3f3f3;
+  color: #15202B;
+  transition: background 0.3s ease-in-out;
+}
+
+.dark {
+  background: #192734;
+  color: #e8e8e8;
 }
 </style>
---
+++ src/components/Content.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="content">
+      <div class="image-grid">
+          <div class="col">
+            <img 
+                src="https://images.unsplash.com/photo-1511732351157-1865efcb7b7b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80" 
+                alt="work-from-home" 
+            />
+          </div>
+          <div class="col">
+            <img 
+                src="https://images.unsplash.com/photo-1491466424936-e304919aada7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1349&q=80" 
+                alt="northern-lights" 
+            />
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['mode']
+}
+</script>
+
+<style>
+.content {
+    padding: 25px;
+}
+
+.content h3 {
+    font-size: 28px;
+    text-transform: uppercase;
+    font-weight: 300;
+    margin-bottom: 25px;
+}
+
+.image-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 15px;
+}
+
+.image-grid img {
+    max-width: 100%;
+    height: auto;
+}
+</style>
